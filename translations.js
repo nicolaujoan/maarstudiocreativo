cb64364e9357a17d7c86b386b65704fda606905a
Joan Nicolau <joan.nicolau@nexialabs.ai>
2026-01-14 13:42:39 +0100

    cambio textos y logos blancos

--- translations.js
+++ translations.js
@@ -14,7 +14,7 @@ const translations = {
         // Services Section
         services_title: "Nuestros Servicios",
         service1_title: "Estrategias",
-        service1_desc: "Buscamos tu lugar en el mercado, investigamos lo que necesita y quiere tu público objetivo  y  diseñamos  estrategias  de  comunicación  conscientes  y  creativas  que conecten de forma efectiva, generen valor y posicionen tu marca.",
+        service1_desc: "Buscamos tu lugar en el mercado, investigamos lo que quiere y necesita tu publico objetivo y diseñamos estrategias de comunicación creativas que conecten de forma efectiva, generen valor y posicionen tu marca.",
         service2_title: "Diseño y Branding",
         service2_desc: "Diseñamos la identidad visual completa, así como la guía de valores y el tono de comunicación de tu negocio. Creamos logos, catálogos, cartas, tarjetas de visita, y todo tipo de material comercial.",
         service3_title: "Creación de contenido",
@@ -30,7 +30,7 @@ const translations = {
         about_title: "Mar Fàbregas",
         about_subtitle: "Fundadora y Directora Creativa",
         about_p1: "Graduada en Periodismo, siempre me ha apasionado la comunicación y contar historias. Mientras cursaba la  carrera,  realicé  prácticas  radiofónicas,  donde  descubrí  que  mi  camino  no  estaba  allí,  aunque  mi  interés por comunicar seguía latente. Una pausa en Londres me permitió comprender el poder del marketing y las redes sociales para cualquier negocio. Allí entendí que trabajar en este ámbito no era solo un trabajo, sino una verdadera vocación. El tiempo volaba cuando me dedicaba a ello.",
-        about_p2: "Durante  diez  años  me  he  formado  en  marketing  y  comunicación  turística,  copywriting  y  otras  disciplinas relacionadas,  lo  que  me  ha  permitido  desarrollar  una  visión  de  360°  y  trabajar  con  grandes  empresas hoteleras  de  lujo  en  Mallorca,  reconocidas  a  nivel  internacional.  Ahora  he  decidido  emprender  mi  propio viaje, ayudando a negocios a crecer con una identidad que refleje su esencia y genere valor.",
+        about_p2: "Durante  diez  años  me  he  formado  en  marketing  y  comunicación  turística,  copywriting  y  otras  disciplinas relacionadas,  lo  que  me  ha  permitido  desarrollar  una  visión  de  360°  y  trabajar  en  grandes  empresas hoteleras  de  lujo  en  Mallorca,  reconocidas  a  nivel  internacional.  Ahora  he  decidido  emprender  mi  propio viaje, ayudando a negocios a crecer con una identidad que refleje su esencia y genere valor.",
 
         // Clients Section
         clients_title: "Ya han confiado en nosotros",
